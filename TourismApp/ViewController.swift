d75f80b8fef102d5f70f300941d929c9698c5115
Zilan <zilan@germanstarmotors.ca>
2020-12-02 20:49:36 -0500

    Initial Commit

---
+++ TourismApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TourismApp
+//
+//  Created by ZILAN OUYANG on 2020-12-02.
+//  Copyright © 2020 ZILAN OUYANG. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
